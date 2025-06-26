1ba611ffffae6a7d203f46511a7de829fadff180
Tamires Campos Pletti <tamiresalvacampos9@gmail.com>
2025-06-26 10:22:31 -0300

    JS

---
+++ Aula04-Operadores/comparisonOperators.js
@@ -0,0 +1,27 @@
+// 👉 Operadores de comparação
+
+console.log("-----------------------------OPERADORES DE COMPARAÇÃO------------------------------");
+
+//--------------------------------------------------------------------------------
+
+const idadeBia = 24;
+const idadeMatheus = 25;
+
+console.log("Bia é mais velha que Matheus? ", idadeBia > idadeMatheus);
+console.log("Bia tem idade igual a de Matheus? ", idadeBia === idadeMatheus);
+console.log("Bia tem a idade diferente da de Matheus? ", idadeBia !== idadeMatheus);
+console.log("Bia é mais nova que Matheus? ", idadeBia < idadeMatheus);
+
+const maioridade = 18;
+
+console.log("Bia é maior de idade? ", idadeBia >= maioridade); // idade da Bia é > ou = a 18 anos?
+
+// -------------------------------------------------------------------------------
+
+const x = 10;
+const y = 20;
+const z = "10";
+
+console.log("O número " + x + " é igual ao número " + y, x === y);
+console.log("O número " + x + " é igual ao número " + z, x == z);
+
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# javascript
- Exercícios de JavaScript do canal CursoEmVídeos
+# Aprendendo JavaScript 👩‍💻
+
+Oi pessoal! Bem-vindos ao curso introdutório da linguagem **JavaScript**. 
+
+As aulas desse curso se encontram dentro da comunidade Dev Completo. Esse curso vai te ensinar desde os conceitos fundamentais da linguagem até métodos mais modernos que são muito úteis no desenvolvimento Web Front-End! 
+
+Aqui nesse repositório você encontra todos os códigos que eu desenvolvi nas aulas sobre JavaScript **(módulos 2 e 3)**, caso queiram baixar ou consultar. Você pode clonar o repositório na sua máquina, ou então baixar através [desse link](https://github.com/bia-dev-completo/aprendendo-javascript/archive/refs/heads/main.zip)
